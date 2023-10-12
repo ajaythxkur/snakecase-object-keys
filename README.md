716590d64529da2816bfd9b5cfe4426a76350208
ajaythxkur <ajaythakur9131@gmail.com>
2023-10-12 20:18:32 +0530

    initial-commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # snakecase-object-keys
 Zero dependency open source code to convert object keys to snake case.
+
+Usage
+-----
+```
+npm install snakecase-object-keys
+
+import { convert } from "snakecase-object-keys";
+const input_object = {
+    "ObjectKey":"value",
+    "anotherKey:[ { "keyInside":"value" } ]
+}
+const output_object = convert(input_object);
+
+```
+
+License
+-------
+MIT
+
+
---
+++ index.js
@@ -0,0 +1,47 @@
+
+
+function snakecase(input) {
+    let output = '';
+    for (let i = 0; i < input.length; i++) {
+        const current_char = input[i];
+        if (current_char === current_char.toUpperCase() && current_char !== current_char.toLowerCase() && i !== 0) {
+            output += '_' + current_char.toLowerCase();
+        } else {
+            output += current_char.toLowerCase(); //First key
+        }
+    }
+    return output;
+}
+function convert_object(input_object){
+    const output_object = {};
+    for(const key in input_object){
+        const snake_case_key = snakecase(key);
+        if(typeof input_object[key] == "object"){
+            if(Array.isArray(input_object[key])){
+                const array_output = convert_array(input_object[key])
+                Object.assign(output_object,{ [snake_case_key]: array_output} )
+                continue;
+            }else{
+                const obj_output = convert_object(input_object[key]);
+                Object.assign(output_object, { [snake_case_key]: obj_output})
+                continue;
+            }
+        }
+        Object.assign(output_object, { [snake_case_key]: input_object[key] })
+    }
+    return output_object;
+}
+
+function convert_array(inputArr){
+    return inputArr.map(v=>{
+        if(typeof v == "object"){
+            if(Array.isArray(v)){
+                return convert_array(v)
+            }
+            return convert_object(v)
+        }
+        return v;
+    })
+}
+
+export const convert = convert_object;
